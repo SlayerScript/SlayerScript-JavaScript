cd7b9fb9f47160e95a11396f5858a341c1a8b0fa
SlayerScript <theslayerscript@gmail.com>
2026-06-26 20:04:04 -0300

    Create tarea_capitulo2.js

---
+++ tarea_capitulo2.js
@@ -0,0 +1,55 @@
+// ⚠️ No modifiques las preguntas.
+// Escribí las respuestas debajo de cada ejercicio y ejecutá el código en tu editor de texto para comprobar que funciona.
+// 1 - a) 
+// Crear una variable que sea de tipo string ya sea con " ", ' ', ` `
+// 1 - b)
+// Crear una variable que sea de tipo number ya sea entero o flotante
+// 1 - c)
+// Crear una variable que sea de tipo booleano true o false
+// Estas tres hay que mostrarlas por consola juntas o por separado
+
+
+// 2 - a) 
+// ¿Estas dos variables son iguales? ¿Por qué?
+
+var edad1 = 10;
+var edad2 = "10";
+
+// 2 - b)
+// ¿Por qué este código da error? ¿Cómo lo solucionarías?
+
+var bruja = "Willow";
+console.log(Bruja);
+
+// 3 - a)
+// Según lo visto en clase, ¿cuál de estas opciones sigue la convención que utilizamos habitualmente en JavaScript?
+
+var ProtagonistaDeSerie = "Buffy";
+var protagonista_de_serie = "Buffy";
+var protagonistadeserie = "Buffy";
+var protagonistaDeSerie = "Buffy";
+
+// 3 - b)
+// Segun lo visto en buenas practicas, ¿Como escribimos adecuadamente esta variable para un valor booleano?
+
+var VIVO = true;
+var estavivo = true;
+var estaVivo = true;
+var verdad = true;
+
+// 4 - a)
+// ¿Esta bien redeclarado este valor?
+
+var protagonista = "Sonic";
+var protagonista = "Shadow";
+console.log(protagonista);
+
+// 4 - b)
+// ¿Está bien reasignado este valor?
+// ¿Qué problema puede traer cambiar el tipo de dato?
+
+var año = 1990;
+var año = "1991";
+console.log(año);
+
+
